c4c72fd9d22ad4cfe09c1d44a3a4de91c4056d43
hrahimzadah <hrahimzadah@hotmail.com>
2022-02-22 18:36:52 -0800

    quiz3

--- quiz3.cpp
+++ quiz3.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <iomanip>
 using namespace std;
 
 int main ()
 {
-    int num1, num2;
+    double num1, num2, num3;
+    double summation;
+    double average;
+
     srand(time(0));
 
     num1 = rand () % 100 ; // 0 - 99
@@ -14,8 +18,24 @@ int main ()
     num2 = rand () % 100 ; // 0 - 99
     cout << num2 << endl;
 
-    if ( num1 > num2 )
-    cout << num1 << "is greater than" << num2 << endl;
-    else 
-    cout << num2 << "is greater than" << num1 << endl;
+    num3 = rand () % 100 ; // 0 - 99
+    cout << num3 << endl;
+
+    cout << "Press 'Enter' to see summation" << endl;
+    cin.get();
+
+    cout << "Adding numbers : " << num1 << "+" << num2 << "+" << num3 << endl;
+
+    summation = num1 + num2 + num3;
+
+    cout << "Total number is: " << summation << endl;
+
+    cout << "Press 'Enter' to see average" << endl;
+
+    average = (num1 + num2 + num3) / 3;
+
+    cout << "Average is:" << setprecision(5) << average << endl;
+
+    return 0;
+
 }
