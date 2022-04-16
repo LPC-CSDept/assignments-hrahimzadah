436d3b88bbc0afa0d6d54fe5a837d8beac392722
hrahimzadah <hrahimzadah@hotmail.com>
2022-04-15 23:26:17 -0700

    Quiz 6-1

--- quiz6-1-1.cpp
+++ quiz6-1-1.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 using namespace std;
 
+void swapTwoValues (int& , int&);
+void swapTwoValues (float&, float&);
+
+int main ()
+{
+    int a = 3, b = 4;
+    cout << "Before swapping int: a = " << a << " b = " << b << endl;
+    cout << "After swapping int: a =" << a << "b = " << b << endl;
+
+return 0;
+}
