54d978d82c76767ecb564e6c81b956eb0f1fb8e7
hrahimzadah <hrahimzadah@hotmail.com>
2022-04-16 21:13:38 -0700

    quiz6-1-3

--- quiz6-1-3.cpp
+++ quiz6-1-3.cpp
@@ -32,3 +32,20 @@ int findMin (int n1, int n2, int n3)
         return n3;
     }
 }
+void printResult (int n1,int n2, int n3, int min)
+{
+    cout << "\n n1:" << n1 << "\n n2:" << n2 << "\n n3:" << n3 << endl;
+    cout << "The minimum value is: " << min << endl;
+}
+
+int main ()
+{
+    int n1 = 0, n2 = 0, n3 = 0;
+    getRandnum (n1,n2,n3);
+
+    cout << n1 << " " << n2 << " " << n3 << endl;
+    int min = findMin (n1,n2,n3);
+    printResult (n1,n2,n3,min);
+
+    return 0;
+}
