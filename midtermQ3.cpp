8835b4c56f980ec4024b1b07c3b12c6ba017fb3a
hrahimzadah <hrahimzadah@hotmail.com>
2022-05-12 08:29:59 -0700

    midtermQ3

--- midtermQ3.cpp
+++ midtermQ3.cpp
@@ -9,3 +9,28 @@ int getRdnum(void)
     int A = rand() % 50 + 1;
     return A;
 }
+int isGreater (int n)
+{
+    if (n>previous)
+    {
+        previous = n;
+        return 1;
+    }
+    else
+    {
+        previous = n;
+        return 0;
+    }
+    return 1;  
+}
+
+int main()
+{
+    int N = 10;
+    ofstream f("numbers.txt");
+    for (int i=0;i<10;i++)
+    {
+        int n = getRdnum();
+
+    }
+}
