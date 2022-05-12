3797bad0ca91db320353b6445d2cbed92939cc2f
hrahimzadah <hrahimzadah@hotmail.com>
2022-05-12 09:05:55 -0700

    midtermQ5

--- midtermQ5.cpp
+++ midtermQ5.cpp
@@ -1,3 +1,32 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
+void getinput (int X, int Y)
+{
+    cout << "Enter the value for the first number: ";
+    cin >> X;
+    cout << "Enter the value for the second number: ";
+    cin >> Y;
+}
+
+int isSame (int X, int Y)
+{
+    if (X == Y)
+    {
+        return 1;
+    }
+}
+
+float intDivision (int X, int Y)
+{
+    if (X > Y)
+    {
+        cout << "\n Divide the first number by the second number...";
+        return float (X) / float (Y);
+    }
+    else
+    {
+        cout << "\n Divide the second number by the first number...";
+        return float (Y) / float (X);
+    }
+}
