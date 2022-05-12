74b06ca9b1fc4247838fb0081bd5c51056289b3f
hrahimzadah <hrahimzadah@hotmail.com>
2022-05-12 09:12:06 -0700

    midtermQ5

--- midtermQ5.cpp
+++ midtermQ5.cpp
@@ -30,3 +30,36 @@ float intDivision (int X, int Y)
         return float (Y) / float (X);
     }
 }
+
+void fileWrite (float result)
+// What this does is opens the file to be able to write inside of.
+{
+    ofstream fw ("D:\\question5.txt,std::ofstream::out");
+    cout << "\n Writing inside of the file...";
+    fw << result << "\n";
+    fw.close();
+}
+
+int main ()
+{
+    int N1, N2;
+    float result;
+
+    getInput (&N1, &N2);
+
+    if (isSame(&N1, &N2)==1)
+    {
+        cout << "\n The numbers are equal to one another";
+        exit;
+    }
+
+    else
+{
+    result = intDivision (&N1, &N2);
+    cout << "\n Dividing both numbers is equal to: " << result;
+}
+
+filewrite(result);
+
+exit;
+}
