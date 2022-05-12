407eeb0cf14ca848e8131558b7a229e8574719d3
hrahimzadah <hrahimzadah@hotmail.com>
2022-05-12 08:38:46 -0700

    midtermQ4

--- midtermQ4.cpp
+++ midtermQ4.cpp
@@ -2,3 +2,12 @@
 #include <fstream>
 using namespace std;
 
+void getInput (int * x, int * y)
+{
+    cout << "Enter the value of number 1: ";
+    cin >> * x;
+    cout << "Enter the value of number 2: ";
+    cin >> * y;
+}
+
+int isSame
