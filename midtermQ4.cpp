f9e9564cfe8a3514d408a0f5c1dfce23a507ba8e
hrahimzadah <hrahimzadah@hotmail.com>
2022-05-12 08:51:35 -0700

    midtermQ4

--- midtermQ4.cpp
+++ midtermQ4.cpp
@@ -13,3 +13,16 @@ int getRdnum(int N1)
     srand (time(NULL));
     return rand() % (101-N1)+N1;
 }
+
+int getRdnum (int N1, int N2)
+{
+    srand (time(NULL));
+    return rand () % (N2-N1)+N1;
+}
+
+int main ()
+{
+    cout << getRdnum () << endl;
+    cout << getRdnum (50) << endl;
+    cout << getRdnum (80,90) << endl;
+} 
