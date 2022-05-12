6f253940600bba9bb569bf1bef984369e5d81601
hrahimzadah <hrahimzadah@hotmail.com>
2022-05-12 08:50:57 -0700

    midtermQ4

--- midtermQ4.cpp
+++ midtermQ4.cpp
@@ -1,3 +1,15 @@
 #include <iostream>
 #include <ctime>
 using namespace std;
+
+int getRdnum()
+{
+    srand (time(NULL));
+    return rand() % 101;
+}
+
+int getRdnum(int N1)
+{
+    srand (time(NULL));
+    return rand() % (101-N1)+N1;
+}
